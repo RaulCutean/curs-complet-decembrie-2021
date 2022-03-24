e66fe4bfbf3861744a56a9135c43ead1e1e400fc
zmedd <zmed.alex@gmail.com>
2022-03-24 20:29:55 +0200

    newsfeed logic

--- social-media-html-css/js/consts.js
+++ social-media-html-css/js/consts.js
@@ -7,6 +7,25 @@ const ENDPOINTS = {
   REGISTER: "/users/register",
   LOGIN: "/users/login",
   GET_ALL_POSTS: "/posts",
+  LIKE_POST: "/posts/like",
+  SHARE_POST: "/posts/share",
+  COMMENT: "/posts/comment",
+  ADD_NEW_POST: "/posts",
 };
 
 const currentTime = Math.floor(new Date().getTime() / 1000);
+
+const months = [
+  "Jan.",
+  "Feb.",
+  "Mar.",
+  "Apr.",
+  "May",
+  "Jun.",
+  "Jul.",
+  "Aug.",
+  "Sep.",
+  "Oct.",
+  "Nov.",
+  "Dec",
+];
