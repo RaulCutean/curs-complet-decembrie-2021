dcaf41471f85e873c33c744de396b1f531ae15a3
zmedd <zmed.alex@gmail.com>
2022-04-07 17:59:04 +0300

    curs 29 tweak

--- curs29/src/index.js
+++ curs29/src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
+const root = createRoot(document.getElementById("root"));
 root.render(<App tab="home" />);
-
-/*
-FCP - First contentful paint (de cand incepe sa incarce pana cand a aparut primul element)
-TTFB - Time to First Byte (dupa cat timp primim primele date) Recomandat < 0.01
-*/
-
-reportWebVitals(console.log);
